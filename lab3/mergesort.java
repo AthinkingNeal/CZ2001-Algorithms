d704dc65046632c286821a7d100c2eba9c0ff016
aThinkingNeal <31988374+AthinkingNeal@users.noreply.github.com>
2019-10-14 22:18:03 +0800

    Update mergesort.java

--- lab3/mergesort.java
+++ lab3/mergesort.java
@@ -40,18 +40,17 @@ public class mergesort {
             // Sort first and second halves
             mergeSortUnmodified(array, first, mid);
             mergeSortUnmodified(array, mid + 1, last);
-
         }
         // Merge the sorted halves
-        mergeUnmodified(array,first,mid,last);
+        merge(array,first,mid,last);
     }
 
     // Merge two subarrays of array[]
     // First subarray is array[first] -- array[mid]
     // Second subarray is array[mid + 1] -- array[last]
 
-    // refer to the Mergesort Appendix in CZ2001 NTULearn for the following merge algorithm
-    public static void mergeUnmodified(int [] array, int first, int mid, int last){
+    // refer to the Mergesort Appendix at CZ2001 NTULearn for the following merge algorithm
+    public static void merge(int [] array, int first, int mid, int last){
         // a indicates the index of the last element of the sorted list
         // b indicates the index of the first element of 2nd half of the list
         int a = first, b = mid + 1, i, temp;
@@ -60,6 +59,11 @@ public class mergesort {
 
         while (a <= mid && b <= last) {
             int cmp = compare(array[a],array[b]);
+
+//            The compare() method of Integer class of java.lang package compares two integer values (x, y) given
+//            as a parameter and returns the value zero if (x==y), if (x < y) then it returns a value less than zero and
+//            if (x > y) then it returns a value greater than zero.
+
             if (cmp > 0) { // array[a] > array[b]
                 temp = array[b++]; // store the key value of the 1st element of the 2nd half
                 for (i = ++mid; i > a; i--) { // shift the 1st half of the array to the right for one slot
@@ -91,18 +95,79 @@ public class mergesort {
 //        datasize  = sc.nextInt();
 //        int [] dataset = generateData(datasize);
 //        for (int i = 0; i < datasize; i++){
-//            System.out.println(dataset[i]);
+//            System.out.print(dataset[i] + " ");
 //        }
-//        System.out.print("\n");
+//        System.out.println("\nSorted list:");
 //        mergeSortUnmodified(dataset,0,datasize-1);
 //        for (int i = 0; i < datasize; i++){
-//            System.out.println(dataset[i]);
+//            System.out.print(dataset[i] + " ");
 //        }
 //    }
 
-    public static void insertionSort(int[] array, int first, int last){
-
+    public static void insertionSort(int[] array, int first,int last){
+        // input array is of n records
+        // we assume n > 1
+        int n = last - first + 1;
+        int temp = 0;
+        for (int i = 1; i < n; i++){
+            for(int j = i; j > 0; j--){
+                if (array[j] < array[j-1]) {
+                    temp = array[j-1];
+                    array[j-1] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
     }
+
+    //    Unit test for data generator + Insertionsort. Test passed.
+//
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Please enter data size: ");
+//        datasize  = sc.nextInt();
+//        int [] dataset = generateData(datasize);
+//        for (int i = 0; i < datasize; i++){
+//            System.out.print(dataset[i] + " ");
+//        }
+//        System.out.println("\nSorted list: ");
+//        insertionSort(dataset,0,datasize - 1);
+//        for (int i = 0; i < datasize; i++){
+//            System.out.print(dataset[i] + " ");
+//        }
+//    }
+
+
+    public static void mergeSortModified(int [] array, int first, int last, int S){
+
+        if (last - first > S){
+            int mid = (first + last) / 2;
+            mergeSortModified(array,first,mid,S);
+            mergeSortModified(array,mid + 1, last, S);
+            merge(array,first,mid,last);
+        }
+        else{
+            insertionSort(array,first,last);
+        }
+    }
+
+//  Unit test for data generator + mergeSortModified. Test unpassed.
+//
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Please enter data size: ");
+//        datasize  = sc.nextInt();
+//        int [] dataset = generateData(datasize);
+//        for (int i = 0; i < datasize; i++){
+//            System.out.print(dataset[i] + " ");
+//        }
+//        System.out.println("\nSorted array: ");
+//        mergeSortModified(dataset,0,datasize-1,2);
+//        for (int i = 0; i < datasize; i++){
+//            System.out.print(dataset[i] + " ");
+//        }
+//    }
+
 
 
 }
